94ea847785292892ff11ff6328be284d0b1c95d0
Evans Ehiorobo <ehioroboevans@gmail.com>
2019-05-11 15:02:26 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const universeOfDiscourse = [
+    "Google", "Alphabet", "Facebook", "Amazon", "Apple", "Microsoft"
+];
+
+function getBigram(word) {
+    let result = [];
+
+    for (let i = 0; i < word.length-1; i++) {
+        result.push(word[i] + word[i+1]);
+    }
+
+    return result;
+}
+
+function getSimilarity(word1, word2) {
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+    const bigram1 = getBigram(word1), bigram2 = getBigram(word2);
+    let similar = [];
+
+    for (let i = 0; i < bigram1.length; i++) {
+        if (bigram2.indexOf(bigram1[i]) > -1) {
+            similar.push(bigram1[i]);
+        }
+    }
+
+    return similar.length / Math.max(bigram1.length, bigram2.length);
+}
+
+function autoCorrect(word) {
+    let maxSimilarity = 0;
+    let mostSimilar = word;
+
+    for (let i = 0; i < universeOfDiscourse.length; i++) {
+        let similarity = getSimilarity(universeOfDiscourse[i], word);
+        if (similarity > maxSimilarity) {
+            maxSimilarity = similarity;
+            mostSimilar = universeOfDiscourse[i];
+        }
+    }
+
+    return maxSimilarity > 0.5 ? mostSimilar : word;
+}
+
+function submit() {
+    let text = document.getElementById("text").value;
+
+    if (text.length > 0) {
+        const lastChar = text[text.length - 1];
+        
+        if (lastChar === " ") {
+            text = text.split(" ");
+            const lastWord = text[text.length - 2];
+            text[text.length - 2] = autoCorrect(lastWord);
+            text = text.join(" ");
+        }
+
+        document.getElementById("text").value = text;
+        document.getElementById("output").innerHTML = "You know: <br/>" + text;
+    }
+    else
+        document.getElementById("output").innerHTML = "";
+}
+
